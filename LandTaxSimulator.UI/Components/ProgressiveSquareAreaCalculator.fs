d235b49d51f45d764c026fa05f01b1d85bc099e0
Junying Chen <jygchen@gmail.com>
2025-05-25 07:21:06 +1000

    Add remove list

--- LandTaxSimulator.UI/Components/ProgressiveSquareAreaCalculator.fs
+++ LandTaxSimulator.UI/Components/ProgressiveSquareAreaCalculator.fs
@@ -10,16 +10,75 @@ module ProgressiveSquareAreaCalculator =
         Rate: float
     }
 
+    type private BracketContainer = {
+        Bracket: Bracket
+        Selected: bool
+    }
+
     let private replaceElementByIndex index newElement list =
         Array.concat [
             (Array.sub list 0 index);
             [| newElement |];
             (Array.sub list (index + 1) (list.Length - (index + 1)))
         ]
+
+    let private removeElementByIndex index list =
+        Array.concat [
+            (Array.sub list 0 index);
+            (Array.sub list (index + 1) (list.Length - (index + 1)))
+        ]
+
+    let private renderBracketViewRow
+      (bracket: Bracket)
+      (editButtonCallback: _ -> unit)
+      (deleteButtonCallback: _ -> unit) = fragment {
+        MudItem'' {
+            xs 4
+            sm 4
+            md 4
+            lg 4
+            xl 4
+            bracket.PricePerSqmThreshold
+        }
+        MudItem'' {
+            xs 4
+            sm 4
+            md 4
+            lg 4
+            xl 4
+            bracket.Rate
+        }
+        MudItem'' {
+            xs 2
+            sm 2
+            md 2
+            lg 2
+            xl 2
+            MudButton'' {
+                Color Color.Primary
+                Variant Variant.Outlined
+                OnClick (fun _ -> editButtonCallback ())
+                "Edit"
+            }
+        }
+        MudItem'' {
+            xs 2
+            sm 2
+            md 2
+            lg 2
+            xl 2
+            MudButton'' {
+                Color Color.Primary
+                Variant Variant.Outlined
+                OnClick (fun _ -> deleteButtonCallback ())
+                "Delete"
+            }
+        }
+      }
     
     let private renderBracketEditRow
       (bracket: Bracket)
-      (editButtonCallback: Bracket -> unit)
+      (saveButtonCallback: Bracket -> unit)
       (editButtonLabel: string) =
         adapt {
             let! currentBracket, setCurrentBracket = cval(bracket).WithSetter()
@@ -60,45 +119,60 @@ module ProgressiveSquareAreaCalculator =
                 MudButton'' {
                     Color Color.Primary
                     Variant Variant.Outlined
-                    OnClick (fun _ -> editButtonCallback(currentBracket))
+                    OnClick (fun _ -> saveButtonCallback(currentBracket))
                     editButtonLabel
                 }
             }
         }
 
     let view () = adapt {
-        let! brackets, setBrackets = cval<Bracket array>([|
-            { PricePerSqmThreshold = 0; Rate = 0.00 };
-            { PricePerSqmThreshold = 1; Rate = 0.01 } |]).WithSetter()
+        let! bracketContainers, setBracketContainers = cval<BracketContainer array>([|
+            { Bracket = { PricePerSqmThreshold = 0; Rate = 0.00 }; Selected = false };
+            { Bracket = { PricePerSqmThreshold = 1; Rate = 0.01 }; Selected = false } |]).WithSetter()
 
-        fragment {
-            MudGrid'' {
-                fragment {
-                    for index in [0..(brackets.Length - 1)] ->
-                        let bracket = brackets[index]
+        MudGrid'' {
+            fragment {
+                for index in [0..(bracketContainers.Length - 1)] ->
+                    let bracketContainer = bracketContainers[index]
+                    match bracketContainer.Selected with
+                    | false ->
+                        renderBracketViewRow
+                            bracketContainer.Bracket
+                            (fun _ ->
+                                replaceElementByIndex
+                                    index
+                                    { bracketContainer with Selected = true }
+                                    bracketContainers
+                                |> setBracketContainers
+                            )
+                            (fun _ ->
+                                removeElementByIndex index bracketContainers
+                                |> setBracketContainers
+                            )
+                    | true ->
                         renderBracketEditRow
-                            bracket
+                            bracketContainer.Bracket
                             (fun changedBracket ->
                                 replaceElementByIndex
                                     index
                                     changedBracket
-                                    brackets
-                                |> setBrackets
+                                    (bracketContainers |> Array.map (fun bc -> bc.Bracket))
+                                |> Array.map (fun b -> { Bracket = b; Selected = false })
+                                |> setBracketContainers
                             )
                             "Save"
-                }
-                hr {}
-                renderBracketEditRow
-                    { PricePerSqmThreshold = 0; Rate = 0.00 }
-                    (fun newBracket ->
-                        Array.concat [ brackets; [| newBracket |] ]
-                        |> setBrackets
-                    )
-                    "Add"
             }
+            hr {}
+            renderBracketEditRow
+                { PricePerSqmThreshold = 0; Rate = 0.00 }
+                (fun newBracket ->
+                    Array.concat [ bracketContainers; [| { Bracket = newBracket; Selected = false } |] ]
+                    |> setBracketContainers
+                )
+                "Add"
         }
 
-        for bracket in brackets do
-            p { $"PricePerSqmThreshold:{bracket.PricePerSqmThreshold} Rate:{bracket.Rate}" }
+        for bracketContainer in bracketContainers do
+            p { $"PricePerSqmThreshold:{bracketContainer.Bracket.PricePerSqmThreshold} Rate:{bracketContainer.Bracket.Rate}" }
     }
 
