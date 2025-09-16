b09334e135d94fe4b115d357086d85fef98e5777
Junying Chen <jygchen@gmail.com>
2025-09-16 22:52:09 +1000

    there is such thing as AValBuilder

--- LandTaxSimulator.UI/Pages/AvalePage.fs
+++ LandTaxSimulator.UI/Pages/AvalePage.fs
@@ -6,25 +6,98 @@ open MudBlazor
 
 module AvalePage =
     let routerView () =
-        let first = cval 0
-        let second = cval 0
+        let firstState = cval 0
+        let secondState = cval 0
+        let thridState = cval 0
+        let fourState = cval 0
+        let fithState = cval 0
+        let sumAval = aval {
+            let! a = firstState
+            let! b = secondState
+            let! c = thridState
+            let! d = fourState
+            let! e = fithState
+            return a + b + c + d + e
+        }
         adapt {
-            let! firstValue, setFirstValue = first.WithSetter()
-            let! secondValue, setSecondValue = second.WithSetter()
-            let sum = firstValue + secondValue
+            let! first, setFirst = firstState.WithSetter()
+            let! second, setSecond = secondState.WithSetter()
+            let! third, setThird = thridState.WithSetter()
+            let! four, setFour = fourState.WithSetter()
+            let! fith, setFith = fithState.WithSetter()
 
-            p { $"firstValue: {firstValue}" }
-            p { $"secondValue: {secondValue}" }
+            let! sum = sumAval
+
+            p { $"firstValue: {first}" }
+            p { $"secondValue: {second}" }
+            p { $"thirdValue: {third}" }
+            p { $"fourValue: {four}" }
+            p { $"fithValue: {fith}" }
             p { $"sum: {sum}" }
 
-            MudButton'' {
-                OnClick (fun _ -> setFirstValue(firstValue + 1))
-                "Increment First"
-            }
+            MudGrid'' {
+                MudItem'' {
+                    xs 12
+                    sm 12
+                    md 12
+                    lg 12
+                    xl 12
+                    MudButton'' {
+                        OnClick (fun _ -> setFirst(first + 1))
+                        "Increment First"
+                    }
 
-            MudButton'' {
-                OnClick (fun _ -> setSecondValue(secondValue + 1))
-                "Increment Second"
+                    MudButton'' {
+                        OnClick (fun _ -> setSecond(second + 1))
+                        "Increment Second"
+                    }
+
+                    MudButton'' {
+                        OnClick (fun _ -> setThird(third + 1))
+                        "Increment Third"
+                    }
+
+                    MudButton'' {
+                        OnClick (fun _ -> setFour(four + 1))
+                        "Increment Four"
+                    }
+
+                    MudButton'' {
+                        OnClick (fun _ -> setFith(fith + 1))
+                        "Increment Fith"
+                    }
+                }
+                MudItem'' {
+                    xs 12
+                    sm 12
+                    md 12
+                    lg 12
+                    xl 12
+                    MudButton'' {
+                        OnClick (fun _ -> setFirst(first - 1))
+                        "Decrement First"
+                    }
+
+                    MudButton'' {
+                        OnClick (fun _ -> setSecond(second - 1))
+                        "Decrement Second"
+                    }
+
+                    MudButton'' {
+                        OnClick (fun _ -> setThird(third - 1))
+                        "Decrement Third"
+                    }
+
+                    MudButton'' {
+                        OnClick (fun _ -> setFour(four - 1))
+                        "Decrement Four"
+                    }
+
+                    MudButton'' {
+                        OnClick (fun _ -> setFith(fith - 1))
+                        "Decrement Fith"
+                    }
+                }
             }
         }
 
